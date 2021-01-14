580c03c6338ccb4be7e878ca9cb3f791a7c21e47
Mateus Santos <mateusdossantos52@gmail.com>
2021-01-14 09:54:53 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/curso/entities/User.java
+++ src/main/java/com/educandoweb/curso/entities/User.java
@@ -13,7 +13,7 @@ public class User implements  Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String nome;
 	private String email;
 	private String phone;
@@ -25,7 +25,7 @@ public class User implements  Serializable{
 		
 	}
 
-	public User(long id, String nome, String email, String phone, String password) {
+	public User(Long id, String nome, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.nome = nome;
@@ -34,11 +34,11 @@ public class User implements  Serializable{
 		this.password = password;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
